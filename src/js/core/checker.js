460971067a244f3ef2c95b842a842a27843f26f4
hecun0000 <1639586328@qq.com>
2018-01-31 23:39:13 +0800

    second

---
+++ src/js/core/checker.js
@@ -0,0 +1,109 @@
+// 检查数独的解决方案
+function checkArray(array){
+    const length = array.length;
+    const marks = new Array(length);
+    marks.fill(true);
+
+    for (let i = 0; i < length-1; i++) {
+        if(!marks[i]){
+            continue;
+        }
+        const v = array[i];
+        
+        if(!v) {
+            marks[i] = false;
+            continue;
+        }
+
+        for (let j = i+1; j < length; j++) {
+            if(v === array[j]){
+                marks[i]=marks[j]=false;
+            }            
+        }
+    }
+    return marks;
+}
+const Toolkit = require("./toolkit");
+class Checker {
+    constructor(matrix){
+        this._matrix = matrix;
+        this._matrixMarks = Toolkit.matrix.makeMatrix(true);
+    }
+
+    get matrixMarks(){
+        return this._matrixMarks;
+    }
+
+    get isSuccess(){
+        return this._success;
+    }
+
+    check(){
+        this.checkRows();
+        this.checkCols();
+        this.checkBoxes();
+
+        // 检查是否成功
+        this._success = this._matrixMarks.every(row=>row.every(mark =>mark))
+        return this._success;
+    }
+
+    checkRows(){
+        for (let rowIndex = 0; rowIndex < 9; rowIndex++) {
+            const row = this._matrix[rowIndex];
+            const marks = checkArray(row);
+            
+            for (let colIndex = 0; colIndex < marks.length; colIndex++) {
+                if(!marks[colIndex]){
+                    this._matrixMarks[rowIndex][colIndex] = false;
+                }                
+            }
+        }
+    }
+
+    checkCols(){
+        const cols=[];
+        for (let colIndex = 0; colIndex < 9; colIndex++) {
+            for (let rowIndex = 0; rowIndex < 9; rowIndex++) {
+                cols[rowIndex] = this._matrix[rowIndex][colIndex];
+                
+            }            
+        }
+
+        const marks = checkArray(cols);
+        for (let rowIndex = 0; rowIndex < 9; rowIndex++) {
+            if (!marks[rowIndex]) {
+                this._matrixMarks[rowIndex][colIndex] = false;                
+            }
+            
+        }
+    }
+
+    checkBoxes(){
+        for (let boxIndex = 0; boxIndex < 9; boxIndex++) {
+            const boxes = Toolkit.box.getBoxCells(this._matrix,boxIndex);
+            const marks = checkArray(boxes);
+            for (let cellIndex = 0; cellIndex < 9; cellIndex++) {
+                if(!marks[cellIndex]){
+                    const {rowIndex,colIndex}=Toolkit.box.convertFromBoxIndex(boxIndex,cellIndex);
+                    this._matrixMarks[rowIndex][colIndex] = false;
+                }
+            }            
+        }
+    }
+}
+
+// const generator = require("./generator");
+// const gen = new generator();
+
+// gen.generate();
+// const matrix = gen.matrix;
+// const checker = new Checker(matrix);
+// const checker2 = new Checker(matrix);
+
+// console.log(checker.check());
+// console.log(checker.matrixMarks);
+// matrix[1][1]=0;
+// matrix[3][5] = matrix[5][3] = 4;
+// console.log(checker2.check());
+// console.log(checker2.matrixMarks);
--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,7 @@
-const toolkit = require("./core/toolkit");
 
-class Grid{
-    constructor(container){
-        this._$container = container;
-    }
 
-    build(){
-        const matrix = toolkit.makeMatrix();
-        const rowGrounpClasses = ["row_g_top","row_g_middle","row_g_bottom"];
-        const colGrounpClasses = ["col_g_left","col_g_center","col_g_right"];
-        const $cells = matrix.map(rowValues => rowValues.map((cellValue,colIndex) => {
-            return $("<span>").text(cellValue).addClass(colGrounpClasses[colIndex%3]);
-        }));
-        
-        const $divArray = $cells.map(($spanArray,rowIndex) =>{
-            return $("<div>").append($spanArray).addClass("row").addClass(rowGrounpClasses[rowIndex%3]);
-        });
 
-        this._$container.append($divArray);
-    }
-
-    layout(){
-        const width = $("span:first",this._$container).width();
-        $("span",this._$container).height(width).css({
-            "line-height":`${width}px`,
-            "font-size": width>32?`${width/2}px`:""
-        })
-    }
-}
-
+const Grid = require("./ui/grid");
 const grid = new Grid($("#container"));
 grid.build();
 grid.layout();
